ca12f0d5c4e22563e598a24e62ea5a58d29a9a9d
Yan <yonkishum@gmail.com>
2023-09-19 14:29:42 -0400

    Update pdfconverter.py
    adding some comments.

--- pdfconverter.py
+++ pdfconverter.py
@@ -1,15 +1,26 @@
+# importing fpdf
 from fpdf import FPDF
+# importing os
 import os
 
+# creating a FPDF object
 pdf = FPDF()
+# setting auto pages to break after a image
 pdf.set_auto_page_break(0)
 
+# listing all the files in a single array
 img_list = [x for x in os.listdir('input_images')]
 
+# looping and sorting the images
 for img in sorted(img_list):
+    # creating a page
     pdf.add_page()
+    # fetching the image
     image = "input_images/" + img
+    # adding the image into the pdf object
     pdf.image(image)
-    
+
+# creating the pdf file in the output
 pdf.output("document_converted.pdf", "F")
+# printing in the console, all tasks completed
 print("completed")
